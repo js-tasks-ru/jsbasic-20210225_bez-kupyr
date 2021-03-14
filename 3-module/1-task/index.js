57c7bbf09e088109d1645997be8d6577c7578da0
ITihonya <itihonya_js@mail.ru>
2021-03-14 11:40:58 +0300

    3-й модуль.Задачи 1-2.На проверку

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,8 @@
 function namify(users) {
-  // ваш код...
+  //новый ассив куда будем складывать иена пользователей
+  let nameUsers = []; 
+    users.forEach(item => {
+    nameUsers.push(item.name)
+  });
+  return nameUsers;
 }
--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -1,3 +1,14 @@
+let arr = [5,15,105,37,24,1002,75,12,44,13,98,32, 3, 8,63, 1];
+
 function filterRange(arr, a, b) {
-  // ваш код...
+  //новый массив куда будем складывать значения попадающие в указанный диапазон
+  const newArray = [];
+   arr.forEach(prop => {
+     //если условие выполняется добавляем значение в новый массив...если нет...идём дальше
+     if(prop >= a && prop <= b){
+       newArray.push(prop);
+     }        
+   })
+  return newArray;
 }
+let filtered = filterRange(arr, 13, 63);
