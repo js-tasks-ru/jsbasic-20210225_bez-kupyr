7b3c58722b57ef5d342eef8b98fff3ae181c8a17
ITihonya <itihonya_js@mail.ru>
2021-03-22 22:31:21 +0300

    4-й модуль. Задачи 1-2.На проверку

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,11 @@
-function makeFriendsList(friends) {
-  // ваш код...
+function makeFriendsList(friends){
+  let bodyTeg = document.querySelector("body")
+  let ulList = document.createElement("ul");      
+  for(let friend of friends){        
+    let liList = document.createElement("li");
+    liList.innerHTML = friend.firstName + " " + friend.lastName;
+    ulList.append(liList);
+  }
+  bodyTeg.append(ulList);
+  return ulList;
 }
--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,13 @@
-function makeDiagonalRed(table) {
-  // ваш код...
+function makeDiagonalRed(table) {  
+  let trCollections = table.rows;
+  let indexTd = 0;
+  for (let tr of trCollections){         
+    let tdColletions = tr.cells;
+    for (let td of tdColletions){
+      if(td.cellIndex === indexTd){
+        td.style.background = "red";
+      }            
+    }
+    indexTd++; 
+  }  
 }
