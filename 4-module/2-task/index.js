2931d9a72372c10280746cfec81ea7f7041f1958
ITihonya <itihonya_js@mail.ru>
2021-03-25 12:23:57 +0300

    4-й модуль. Задачи 1-2.На проверку.Коррект 2

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,10 +1,7 @@
 function makeDiagonalRed(table) {  
   let trCollections = table.rows;  
   for(let i = 0; i < trCollections.length; i++){
-    console.log(trCollections[i])
     let tdCollections = trCollections[i].cells;
-    if(tdCollections[i].cellIndex === i){
-      tdCollections[i].style.background = "red";        
-    }
+    tdCollections[i].style.background = "red";
   }
 }
