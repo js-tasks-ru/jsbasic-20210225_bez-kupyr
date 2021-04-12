f57e42f4c33a05d95b1f686136c18c3cb4f86e0a
ITihonya <itihonya_js@mail.ru>
2021-04-12 23:34:56 +0300

    6-й модуль. Задача 1.На проверку

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -1,18 +1,63 @@
-/**
- * Компонент, который реализует таблицу
- * с возможностью удаления строк
- *
- * Пример одного элемента, описывающего строку таблицы
- *
- *      {
- *          name: 'Ilia',
- *          age: 25,
- *          salary: '1000',
- *          city: 'Petrozavodsk'
- *      }
- *
- */
+
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+    this.removeUsers = this.removeUsers();
+  }
+  getTemplateThead(){
+    return `<thead>
+      <tr>
+        <th>Имя</th>
+        <th>Возраст</th>
+        <th>Зарплата</th>
+        <th>Город</th>
+        <th></th>
+      </tr>
+    </thead>
+    `
+  }
+  
+  getTemplateTr(user){
+    return `<tr class="users">
+      <th class="name">${user.name}</th>
+      <th class="age">${user.age}</th>
+      <th class="salary">${user.salary}</th>
+      <th class="city">${user.city}</th>
+      <td><button class="remove-button">X</button></td>
+    </tr>
+    `
+  }
+
+  removeUsers(){
+    let users = [...document.querySelectorAll(".users")];
+    console.log(1);
+    users.forEach(user => {
+      const removeButton = user.querySelector(".remove-button");
+      const removeUser = () => {
+        user.remove();
+        removeButton.removeEventListener('click',removeUser);
+      }
+      removeButton.addEventListener('click',removeUser);
+    })
+  }
+  
+  render(){
+    let table = document.createElement('table');
+    table.innerHTML = this.getTemplateThead();
+    let tbody = document.createElement('tbody');
+
+    for(let user of this.rows){
+      tbody.insertAdjacentHTML("beforeend",this.getTemplateTr(user));  
+    }
+    table.appendChild(tbody);
+    console.log(table);
+    //document.body.appendChild(table);
+    //let users = [...document.querySelectorAll(".users")];  
+    this.removeUsers(this.users);
+    //console.log(users);
+    return table;    
   }
 }
+
+
